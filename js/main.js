758ce4740b77f6b2e883897c0c77392a369c6594
Jason Tesar <37487028+jasonTesar@users.noreply.github.com>
2018-12-20 09:29:13 -0700

    successfully passes fetch results and clicked card id to addDetails()

--- js/main.js
+++ js/main.js
@@ -1,60 +1,55 @@
 const gallery = document.querySelector('#gallery');
 const overlay = document.querySelector('#overlay');
 const cards = document.querySelectorAll('.employee-card');
-
-
-// ------------------------------------
-// EVENT LISTENERS
-// ------------------------------------
-
-// Adds listener to each individual card to avoid slow response from event bubbling
-for (let i = 0; i < cards.length; i += 1) {
-  cards[i].addEventListener('click', (event) => {
-    overlay.style.display = 'flex';
-  });
-}
-
-overlay.addEventListener('click', () => {
-  overlay.style.display = 'none';
-});
+let results = [];
 
 // ------------------------------------
 // FETCH FUNCTIONS
 // ------------------------------------
 
-// Fetches employee data from randomuser website,
-// parses it from JSON into JS,
-// and sends the results to the makeGallery function.
+// Fetches employee data from random user website, parses it from JSON into JS, and sends the results to the makeGallery function.
 // CONSIDER: also calling a makeOverlay function and putting brackets around both function calls?
 // The makeOverlay function would be called from a click event listener on the gallery cards?
 fetch('https://randomuser.me/api/?results=12&nat=us')
-  .then(response => response.json())
-  .then(response => {
-    makeGallery(response.results);
-    // addOverlayDetails(response.results)
-  })
+.then(response => response.json())
+.then(response => {
+  makeGallery(response.results);
+  results = response.results;
+})
+
+// ------------------------------------
+// EVENT LISTENERS
+// ------------------------------------
+
+// Adds listener to each individual card (to avoid slow response from event bubbling),
+// captures card id, shows the overlay, then calls addDetails() and passes it
+// the fetch results and the card id to build the overlay details.
+for (let i = 0; i < cards.length; i += 1) {
+  cards[i].addEventListener('click', (event) => {
+    let id = event.target.id;
+    if (id == null || id == '') {
+      id = event.target.parentNode.id;
+      if (id == null || id == '') {
+        id = event.target.parentNode.parentNode.id;
+      }
+    }
+    overlay.style.display = 'flex';
+    // console.log(id);
+    addDetails(results, id);
+  });
+}
+
+// Hides the overlay when clicked
+overlay.addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
+
 
 // ------------------------------------
 // HELPER FUNCTIONS
 // ------------------------------------
 
-// Creates the gallery grid of employee cards
-// function makeGallery(results) {
-//   let employeeCards = '';
-//   for (let i = 0; i < results.length; i += 1) {
-//     employeeCards += `
-//       <div class="employee-card">
-//         <img class="profile-pic" alt="profile picture" src="${results[i].picture.large}">
-//         <div class="employee-info">
-//           <h4>${results[i].name.first + ' ' + results[i].name.last}</h4>
-//           <p>${results[i].email}</p>
-//           <p class="city">${results[i].location.city}</p>
-//         </div>
-//       </div>
-//     `;
-//     gallery.innerHTML = employeeCards;
-//   };
-// }
+// Creates the intial gallery of employee cards
 function makeGallery(results) {
   for (let i = 0; i < results.length; i += 1) {
     let employeeCard = '';
@@ -68,4 +63,15 @@ function makeGallery(results) {
     `;
     cards[i].innerHTML = employeeCard;
   };
+}
+
+// Adds the details of the clicked employee to the overlay
+function addDetails(results, id) {
+  
+  console.log(id);
+  // let details = `
+  //
+  // `;
+  //
+  // id.innerHTML = details;
 }
